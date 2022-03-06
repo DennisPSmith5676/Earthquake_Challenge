a92b9a560bb4177d924db100bc6ecaa6fa341430
DennisPSmith5676 <dsmith5676@aol.com>
2022-03-06 11:30:46 -0500

    add earthquake data

--- static/js/logic.js
+++ static/js/logic.js
@@ -2,15 +2,9 @@
 console.log("It's working");
 
 // Create the map object with a center and zoom level.
-let map = L.map("mapid").setView([40.7, -94.5], 4);
+// let map = L.map("mapid").setView([40.7, -94.5], 4);
 
-// Create the map object with a center and zoom level.
-// let map = L.map("mapid", {
-// 	center: [
-// 	  40.7, -94.5
-// 	],
-// 	zoom: 4
-//       });
+
 
 // tile layer
 // We create the tile layer that will be the background of our map.
@@ -40,38 +34,69 @@ let satelliteStreets = L.tileLayer(
   }
 );
 
-// Then we add our 'graymap' tile layer to the map.
-streets.addTo(map);
+let dark = L.tileLayer(
+	"https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
+	{
+	  attribution:
+	    'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+	  maxZoom: 18,
+	  id: "mapbox/dark-v10",
+	  tileSize: 512,
+	  zoomOffset: -1,
+	  accessToken: API_KEY,
+	}
+      );
+	// Create a base layer that holds both maps.
+	let baseMaps = {
+		Street: streets,
+		Dark: dark,
+		satellite: satelliteStreets,
+	      };
+	   
+	      // Create the map object with center, zoom level and default layer.
+let map = L.map('mapid', {
+	center: [30, 30],
+	zoom: 2,
+	layers: [streets]
+    });
+    // Pass our map layers into our layers control and add the layers control to the map.
+L.control.layers(baseMaps).addTo(map);
+
+// // Then we add our 'graymap' tile layer to the map.
+// streets.addTo(map);
 
 //  Add a marker to the map for Los Angeles, California.
 // https://leafletjs.com/SlavaUkraini/reference.html#circlemarker
-let marker = L.circleMarker([34.0522, -118.2437]).addTo(map);
+// let marker = L.circleMarker([34.0522, -118.2437]).addTo(map);
 
 // Add GeoJSON data.
-let sanFranAirport = {
-  type: "FeatureCollection",
-  features: [
-    {
-      type: "Feature",
-      properties: {
-        id: "3469",
-        name: "San Francisco International Airport",
-        city: "San Francisco",
-        country: "United States",
-        faa: "SFO",
-        icao: "KSFO",
-        alt: "13",
-        "tz-offset": "-8",
-        dst: "A",
-        tz: "America/Los_Angeles",
-      },
-      geometry: {
-        type: "Point",
-        coordinates: [-122.375, 37.61899948120117],
-      },
-    },
-  ],
-};
+// let sanFranAirport = {
+//   type: "FeatureCollection",
+//   features: [
+//     {
+//       type: "Feature",
+//       properties: {
+//         id: "3469",
+//         name: "San Francisco International Airport",
+//         city: "San Francisco",
+//         country: "United States",
+//         faa: "SFO",
+//         icao: "KSFO",
+//         alt: "13",
+//         "tz-offset": "-8",
+//         dst: "A",
+//         tz: "America/Los_Angeles",
+
+
+
+//       },
+//       geometry: {
+//         type: "Point",
+//         coordinates: [-122.375, 37.61899948120117],
+//       },
+//     },
+//   ],
+// };
 
 // point to layer funtion example
 // L.geoJson(data, {
@@ -88,26 +113,31 @@ let sanFranAirport = {
 // 	 }
 //  }).addTo(map);
 //
-L.geoJSON(sanFranAirport, {
-  onEachFeature: function (feature, layer) {
-    layer.bindPopup(
-      "<h4>" +
-        feature.properties.city +
-        "</h4>" +
-        "<hr>" +
-        "<h6>faa code</h6>" +
-        feature.properties.faa
-    );
-  },
-}).addTo(map);
+// L.geoJSON(sanFranAirport, {
+//   onEachFeature: function (feature, layer) {
+//     layer.bindPopup(
+//       "<h4>" +
+//         feature.properties.city +
+//         "</h4>" +
+//         "<hr>" +
+//         "<h6>faa code</h6>" +
+//         feature.properties.faa
+//     );
+//   },
+// }).addTo(map);
 
 //     link to airport data
-let airportData =
-  "https://raw.githubusercontent.com/DennisPSmith5676/Earthquake_Challenge/main/static/data/majorAirports.json";
+let earthquakeData =
+  "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
 // Grabbing our GeoJSON data.
-d3.json(airportData).then(function (data) {
+d3.json(earthquakeData).then(function (data) {
   console.log(data);
   // Creating a GeoJSON layer with the retrieved data.
   L.geoJSON(data).addTo(map);
 });
+// Retrieve the earthquake GeoJSON data.
+// d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
+//   // Creating a GeoJSON layer with the retrieved data.
+//   L.geoJSON(data).addTo(map);
+// });
